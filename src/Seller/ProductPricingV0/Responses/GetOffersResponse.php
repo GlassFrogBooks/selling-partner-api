ca30f5d2d8a126748b5e137a78920e46b8bb7ca9
Amerigo Politano <28787066+apolitano1@users.noreply.github.com>
2024-11-23 12:03:37 -0500

    fix errorList bug

--- src/Seller/ProductPricingV0/Responses/GetOffersResponse.php
+++ src/Seller/ProductPricingV0/Responses/GetOffersResponse.php
@@ -11,16 +11,18 @@ declare(strict_types=1);
 namespace SellingPartnerApi\Seller\ProductPricingV0\Responses;
 
 use SellingPartnerApi\Response;
-use SellingPartnerApi\Seller\ProductPricingV0\Dto\ErrorList;
+use SellingPartnerApi\Seller\ProductPricingV0\Dto\Error;
 use SellingPartnerApi\Seller\ProductPricingV0\Dto\GetOffersResult;
 
 final class GetOffersResponse extends Response
 {
+    protected static array $complexArrayTypes = ['errors' => Error::class];
+
     /**
      * @param  ?ErrorList  $errors  A list of error responses returned when a request is unsuccessful.
      */
     public function __construct(
         public readonly ?GetOffersResult $payload = null,
-        public readonly ?ErrorList $errors = null,
+        public readonly ?array $errors = null,
     ) {}
 }
